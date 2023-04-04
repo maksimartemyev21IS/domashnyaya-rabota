b5627d4422e77eccc3f69327e43a4343fa31c995
maksimartemyev21IS <123102326+maksimartemyev21IS@users.noreply.github.com>
2023-04-04 16:09:14 +0300

    Add files via upload

---
+++ santimetri.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Я могу перевести число из см в дециметры или метры?")
+        Dim input, sm, dm, m, ost As Decimal
+        input = Console.ReadLine()
+        m = input \ 100
+        ost = input Mod 100
+        dm = ost \ 10
+        sm = ost Mod 10
+        Console.WriteLine("{0} это {1} м, {2} дм, {3} см", input, m, dm, sm)
+    End Sub
+End Module
